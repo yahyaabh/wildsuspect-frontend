540e189d1ea455db499f0a87f5ae47a0784bf479
yahya <yehia.abu.alhassan.01@gmail.com>
2025-04-21 18:15:37 +0300

    add room ui to welcome

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router";
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 import './index.css'
 import Welcome from "./Welcome";
-
+import Rooms from "./Rooms";
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Welcome/>} />
-    </Routes>
+      <Route path="/rooms"  element={<Rooms/>}/>
+    </Routes> 
   </BrowserRouter>
 )
