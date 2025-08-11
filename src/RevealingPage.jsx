274b1d2e981243267ef25a8e1df9983e86357ba0
yahya <yehia.abu.alhassan.01@gmail.com>
2025-08-11 23:43:10 +0300

    navigate to results

--- src/RevealingPage.jsx
+++ src/RevealingPage.jsx
@@ -4,24 +4,31 @@ import {useContext , useState} from  'react'
 import { PlayerContext } from './playerContext';
 import {RoomContext} from './roomContext';
 import { FaLongArrowAltRight } from "react-icons/fa";
-
-
+import { Link, useNavigate } from 'react-router';
 function RevealingPage() {
  const {PlayerData}= useContext(PlayerContext);
+ const [guess,setGuess] = useState("");
  const [animals,setAnimals] = useState([]);
  const {roomData} = useContext(RoomContext);
  const [impostor,setImpostor] = useState(null);
+const navigate = useNavigate();
 
  let handlePickAnimal = (animal) => {
     //emit to backend the user picked an animal
- }
+    if(animal == roomData.animal) {
+        setGuess("correct");
+    }
+    else {
+        setGuess("wrong");
+    }
+    socket.emit("impostorPickedAnimal",{animal:animal});
+}
 
  let getImpostor = () => {
     console.log(roomData)
     roomData.players.forEach((player) => {
         if(player.knowsAnimal == false) {
             setImpostor(player);
-            console.log(player);
         }
     })
  }
@@ -36,8 +43,13 @@ function RevealingPage() {
         //handle the animals array sent from backend
         setAnimals(arr);
     })
+    //when the impostor has guessed, navigate to results page
+    socket.on("impostorHasGuessed", () => {
+        navigate("../results");
+    })
     return () => {
         socket.off("animalsArraySent");
+        socket.off("impostorHasGuessed");
     }
  },[])
 
@@ -48,11 +60,8 @@ function RevealingPage() {
         { PlayerData.knowsAnimal ?
     <div className='h-screen w-screen flex flex-col items-center justify-around bg-lico'>
         <p className=' text-xl text-white font-semibold'>the impostor was:</p>
-        <p className='text-2xl font-bold text-white bg-bordo p-2 m-4  rounded-md fade-in'>😈 {impostor?impostor.name:""}</p> 
-        <div className='bg-melon     p-2  rounded-md text-white text-lg flex flex-row items-center justify-between w-2/6'>
-            <p>results</p>
-            <FaLongArrowAltRight/>
-            </div>    
+        <p className='text-2xl font-bold text-white bg-bordo p-2 m-4  rounded-md fade-in'>😈 {impostor?impostor.name:""}</p>     
+            
     </div>
 
     :
@@ -62,9 +71,16 @@ function RevealingPage() {
         <p className='text-xl font-bold text-white my-4'>guess what the animal was:</p>
            <div className='flex flex-col items-center justify-between '>
                 {animals.map((animal,index) => (
-                    <button key={index} className='font-mono text-white font-medium bg-bordo p-2 m-2 rounded-md w-full' onClick={() => handlePickAnimal(animal)}>{animal}</button>
+                    <button className={`font-mono text-white font-medium bg-bordo p-2 m-2 rounded-md w-full `} key={index}  onClick={() => handlePickAnimal(animal)}>{animal}</button>
                 ))}
             </div>
+         {guess === "correct" ? 
+            <p className='text-5xl mt-2 fade-in'>✅</p>
+         : guess === "wrong" ? 
+         <p className='text-5xl mt-2 fade-in'>❌</p>
+          : 
+          <p></p>}       
+
     </div>
 }
     </>
